2e5cea11651c50a249ec6c94f4aef39f647b3067
Anna Petrenko <anyutkapaa230205@gmail.com>
2023-11-27 01:29:20 +0200

    Event Class Enhancements

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -50,3 +50,77 @@ public:
         return *this;
     }
 
+
+    int getMaxSeats() const { return maxSeats; }
+    int getNumRows() const { return numRows; }
+    int getNumZones() const { return numZones; }
+    int getSeatsPerRow() const { return seatsPerRow; }
+    const char* getLocationName() const { return locationName; }
+
+    void displayInfo() const {
+        std::cout << "Location: " << locationName << "\nMax Seats: " << maxSeats << "\nNum Rows: " << numRows
+            << "\nNum Zones: " << numZones << "\nSeats Per Row: " << seatsPerRow << std::endl;
+    }
+
+    void processInfo() {
+
+    }
+};
+
+class Event {
+private:
+    std::string eventName;
+    std::string eventDate;
+    std::string eventTime;
+    static const int maxEventNameLength = 50;
+
+public:
+
+    Event() : eventName(""), eventDate(""), eventTime("") {}
+
+    Event(const std::string& eventName, const std::string& eventDate, const std::string& eventTime)
+        : eventName(eventName), eventDate(eventDate), eventTime(eventTime) {}
+
+
+    Event(const Event& other)
+        : eventName(other.eventName), eventDate(other.eventDate), eventTime(other.eventTime) {}
+
+
+    ~Event() {}
+
+
+    Event& operator=(const Event& other) {
+        if (this != &other) {
+            eventName = other.eventName;
+            eventDate = other.eventDate;
+            eventTime = other.eventTime;
+        }
+        return *this;
+    }
+
+
+    std::string getEventName() const { return eventName; }
+    std::string getEventDate() const { return eventDate; }
+    std::string getEventTime() const { return eventTime; }
+
+
+    void displayInfo() const {
+        std::cout << "Event Name: " << eventName << "\nEvent Date: " << eventDate
+            << "\nEvent Time: " << eventTime << std::endl;
+    }
+
+    void processInfo() {
+
+    }
+
+
+    bool operator==(const Event& other) const {
+        return eventName == other.eventName && eventDate == other.eventDate && eventTime == other.eventTime;
+    }
+
+    bool operator<(const Event& other) const {
+
+        return false;
+    }
+};
+
